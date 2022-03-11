277ef4e30bf0d87fb65d480d8dbbf77e1dbf7b4b
Eric Coscolluela <65707641+VanCityEric@users.noreply.github.com>
2022-03-11 11:06:09 -0800

    added some animations

--- client/src/pages/Register/UserType.js
+++ client/src/pages/Register/UserType.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 
 const UserType = ({ setUserType }) => {
   let navigate = useNavigate();
@@ -18,8 +19,6 @@ const UserType = ({ setUserType }) => {
     navigate("/register");
   };
 
- 
-
   return (
     <Fragment>
       <div className="auth-container center-content page-width page-height">
@@ -30,25 +29,31 @@ const UserType = ({ setUserType }) => {
           </h2>
           <div className="user-types-containter center-content">
             <div
-              className="user-type user-general"
+              className="user-type user-photographer"
               onClick={setPhotographerHandler}
             >
               Photographer
             </div>
             <div
-              className="user-type user-photographer "
+              className="user-type user-enthusiast "
               onClick={setEnthusiastHandler}
             >
               Enthusiast
             </div>
-            <div
-              className="user-type user-enthusiast"
-              onClick={setStudentHandler}
-            >
+            <div className="user-type user-student" onClick={setStudentHandler}>
               Student
             </div>
           </div>
-          <p className="text-white text-bold" onClick={() => navigate("/login")}>
+          {/* <p
+            className="text-white text-bold p-link"
+            onClick={() => navigate("/register")}
+          >
+            Skip for now{" "}
+          </p> */}
+          <p
+            className="text-white text-bold p-link"
+            onClick={() => navigate("/login")}
+          >
             <FontAwesomeIcon
               className="fa-arrow-left-long"
               icon={faArrowLeftLong}
